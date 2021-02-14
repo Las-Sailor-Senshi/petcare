1205bba502df20e6fa85c2d4e1b52c09ea95c6e4
luis-barrera <luisvaldovinosbarrera@gmail.com>
2021-02-14 13:57:23 -0600

    Agrega footer

--- admin/panel.php
+++ admin/panel.php
@@ -1458,14 +1458,8 @@
     </section>
     <!-- /.content -->
   </div>
-  <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.1.0-rc
-    </div>
-  </footer>
+
+  <?php include_once('../footer.php');?>
 
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
---
+++ footer.php
@@ -0,0 +1,3 @@
+<footer class="main-footer">
+    <strong>Copyright &copy; 2021. Hecho con ❤️ por Las Sailors Senshi</strong>
+</footer>
