179d8cc1e65fd1958b97b50178072b73335fa8aa
luis-barrera <luisvaldovinosbarrera@gmail.com>
2021-02-13 23:28:22 -0600

    Ya se puede hacer operaciones en la bd

--- admin/db_ecommerce.php
+++ admin/db_ecommerce.php
@@ -1,6 +1,6 @@
 <?php
-    $host="35.208.98.110";
-    $user="phpmyadmin";
-    $dbpass="alucharHermanas";
+    $host="35.208.98.110:3306";
+    $user="website1";
+    $dbpass="XzzIKn6Xrn1p8I";
     $db="mydb";
 ?>
