483c6c36835bb36f49913c14ee0e0f42cfb1f9af
luis-barrera <luisvaldovinosbarrera@gmail.com>
2021-02-16 22:57:50 -0600

    Cambios en ventas.php
    - Formateo del texto

--- admin/ventas.php
+++ admin/ventas.php
@@ -17,61 +17,60 @@
       <div class="row">
         <div class="col-12">
           <div class="card">
-              <!-- /.card-header -->
-              <div class="card-body">
-                <table id="example1" class="table table-bordered table-striped">
-                  <thead>
-                  <tr>
-                    <th>ID</th>
-                    <th>Guía de Envio</th>
-                    <th>Link de tracking</th>
-                    <th>Ver Detalle</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <?php
-                  include_once "db_ecommerce.php";
-                  $con=mysqli_connect($host, $user, $dbpass, $db);
+            <!-- /.card-header -->
+            <div class="card-body">
+              <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Guía de Envio</th>
+                  <th>Link de tracking</th>
+                  <th>Ver Detalle</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                include_once "db_ecommerce.php";
+                $con=mysqli_connect($host, $user, $dbpass, $db);
 
-                  $query="SELECT idOrden, numGuia FROM Ordenes;";
+                $query="SELECT idOrden, numGuia FROM Ordenes;";
 
-                  $res= mysqli_query($con,$query);
+                $res= mysqli_query($con,$query);
 
-                  while ($row= mysqli_fetch_assoc($res)){
-                  ?>
-                  <tr>
-                    <td>Venta #<?php echo $row['idOrden']?></td>
-                    <td><?php echo $row['numGuia']?></td>
-                    <td><a href="https://www.fedex.com/es-mx/home.html" target="_blank">Link de Tracking</a></td>
-                    <td>
-                    <a class="btn btn-info btn-sm" href="detallesVenta.php?id=<?php echo $row=['idOrden']?>" role="button">Ver Detalles</a>
-                    </td>
-                  </tr>
-                  <?php
-                  }
-                  ?>
-                  </tbody>
-                  <tfoot>
-                  <tr>
-                    <th>ID</th>
-                    <th>Guía de Envio</th>
-                    <th>Link de tracking</th>
-                    <th>Ver Detalle</th>
-                  </tr>
-                  </tfoot>
-                </table>
-              </div>
-              <!-- /.card-body -->
+                while ($row= mysqli_fetch_assoc($res)){
+                ?>
+                <tr>
+                  <td>Venta #<?php echo $row['idOrden']?></td>
+                  <td><?php echo $row['numGuia']?></td>
+                  <td><a href="https://www.fedex.com/es-mx/home.html" target="_blank">Link de Tracking</a></td>
+                  <td>
+                  <a class="btn btn-info btn-sm" href="detallesVenta.php?id=<?php echo $row=['idOrden']?>" role="button">Ver Detalles</a>
+                  </td>
+                </tr>
+                <?php
+                }
+                ?>
+                </tbody>
+                <tfoot>
+                <tr>
+                  <th>ID</th>
+                  <th>Guía de Envio</th>
+                  <th>Link de tracking</th>
+                  <th>Ver Detalle</th>
+                </tr>
+                </tfoot>
+              </table>
             </div>
-            <!-- /.card -->
+            <!-- /.card-body -->
           </div>
-          <!-- /.col -->
+          <!-- /.card -->
         </div>
-        <!-- /.row -->
+        <!-- /.col -->
       </div>
-      <!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
+      <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+  </section>
+  <!-- /.content -->
+</div>
 
