4ee0f34873dd826f27e6d0727284987f32b7b8d2
SebastianIslas <sebasislasbecerra@gmail.com>
2021-03-06 00:37:12 -0600

    Video15_ Paginador, barra de busqueda y arreglo de mostrar solo 1 vez los productos que tienen mas de 1 imagen

--- index.php
+++ index.php
@@ -37,9 +37,10 @@
                         </li>
                     </ul>
                     <!-- SEARCH FORM -->
-                    <form class="form-inline ml-3">
+                    <form class="form-inline ml-3" action="index.php?modulo=productos">
                         <div class="input-group input-group-sm">
-                            <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+                            <input class="form-control form-control-navbar bg-gray" type="search" placeholder="Search" aria-label="Search" name="nombre" value = <?php echo $_REQUEST['nombre']??'';?>>
+                            <input type="hidden" name="modulo" value="productos">
                             <div class="input-group-append">
                                 <button class="btn btn-navbar" type="submit">
                                     <i class="fas fa-search"></i>
@@ -139,8 +140,37 @@
                 </nav> 
                 <div class="row mt-1">
                     <?php
+                        //ONLY_FULL_GROUP_BY Se desactivo desde mysql ya que la consulta no funcionaba 
+                        // con el GROUP BY, esto se menciona en comentario de video15
+                        //https://es.stackoverflow.com/questions/158552/error-relacionado-con-sql-mode-only-full-group-by-al-ejecutar-consulta-mysql 
+                        //Lo desactive con lo que sale en el link de arriba
                         include_once "admin/db_ecommerce.php";
                         $con = mysqli_connect($host, $user, $dbpass, $db);
+                        $where=" where 1=1";
+                        $nombre = mysqli_real_escape_string($con,$_REQUEST['nombre']??'');
+
+                        if(empty($nombre)==false){
+                            $where ="and nomProducto like '%".$nombre."%'";
+                        }
+
+                        $queryCuenta='SELECT COUNT(*) as cuenta FROM Productos  $where ;';
+                        $resCuenta=mysqli_query($con,$queryCuenta);
+                        $rowCuenta=mysqli_fetch_assoc($resCuenta);
+                        $totalRegistros=$rowCuenta['cuenta'];
+
+                        $elementosPorPagina = 3;
+                        $totalPaginas=ceil($totalRegistros/$elementosPorPagina);
+                        $paginaSel=$_REQUEST['pagina']??false;
+                        
+                        if($paginaSel==false){
+                            $inicioLimite=0;
+                            $paginaSel=1;
+                        }else{
+                            $inicioLimite=($paginaSel-1)*$elementosPorPagina;
+                        }
+                        $limite ="limit $inicioLimite,$elementosPorPagina ";
+
+
                         $query = "SELECT 
                         p.idProducto,
                         p.nomProducto,
@@ -149,7 +179,11 @@
                         ip.imagenProducto
                         FROM
                         Productos AS p
-                        INNER JOIN ImagenesProductos AS ip ON ip.idProducto = p.idProducto";
+                        INNER JOIN ImagenesProductos AS ip ON ip.idProducto = p.idProducto
+                        $where
+                        GROUP BY p.idProducto
+                        $limite
+                        ";
                         $res = mysqli_query($con, $query);
                         while($row = mysqli_fetch_assoc($res)){
                     ?>
@@ -173,6 +207,50 @@
                         }
                     ?>
                 </div>
+                <?php
+                if($totalPaginas>0){
+                ?>
+                    <nav aria-label="Page navigation">
+                      <ul class="pagination">
+                        <?php
+                            if( $paginaSel!=1 ){
+                        ?>
+                        <li class="page-item">
+                          <a class="page-link" href="index.php?modulo=productos&pagina=<?php echo ($paginaSel-1); ?>" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                            <span class="sr-only">Previous</span>
+                          </a>
+                        </li>
+                        <?php
+                        }
+                        ?>
+
+                        <?php
+                        for($i=1;$i<=$totalPaginas;$i++){
+                        ?>
+                        <li class="page-item <?php echo ($paginaSel==$i)?" active ":" "; ?>">
+                            <a class="page-link" href="index.php?modulo=productos&pagina=<?php echo $i; ?>"><?php echo $i; ?></a>
+                        </li>
+                        <?php
+                        }
+                        ?>
+                        <?php
+                            if( $paginaSel!=$totalPaginas ){
+                        ?>
+                        <li class="page-item">
+                          <a class="page-link" href="index.php?modulo=productos&pagina=<?php echo ($paginaSel+1); ?>" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                            <span class="sr-only">Next</span>
+                          </a>
+                        </li>
+                        <?php
+                            }
+                        ?>
+                      </ul>
+                    </nav>
+                <?php
+                }
+                ?>
          </div>
      </div>
  </div>    
