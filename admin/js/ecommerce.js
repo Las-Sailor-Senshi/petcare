06d94dcfcbf86bc62945b220234c0f4b66ac41c3
Samuel Medina <68279149+MedinaSamuel@users.noreply.github.com>
2021-03-07 23:05:29 -0600

    Update ecommerce.js

--- admin/js/ecommerce.js
+++ admin/js/ecommerce.js
@@ -2,6 +2,27 @@
 var nombreRec=$("#nombreRec").val();
 var emailRec=$("#emailRec").val();
 var direccionRec=$("#direccionRec").val();
+
+$(document).ready(function () {
+    $("#agregarCarrito").click(function (e) { 
+        e.preventDefault();
+        var id=$(this).data('id');
+        var nombre=$(this).data('nombre');
+        var web_path=$(this).data('web_path');
+        var cantidad=$("#cantidadProducto").val();
+        $.ajax({
+            type: "post",
+            url: "ajax/agregarCarrito.php",
+            data: {"id":id,"nombre":nombre,"web_path":web_path,"cantidad":cantidad},
+            dataType: "json",
+            success: function (response) {
+                var cantidad=Object.keys(response).length;
+                $("#badgeProducto").text(cantidad);
+            }
+        });
+    });
+});
+
 $("#jalar").click(function(e){
     var nombreCli=$("#nombreCli").val();
     var emailCli=$("#emailCli").val();
