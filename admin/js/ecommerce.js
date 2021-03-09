464628c81368fd0f160a38536c72d20e2929d0cb
Samuel Medina <68279149+MedinaSamuel@users.noreply.github.com>
2021-03-08 21:30:42 -0600

    Update ecommerce.js

--- admin/js/ecommerce.js
+++ admin/js/ecommerce.js
@@ -1,37 +1,4 @@
-<<<<<<< HEAD
-///Esto va antes de cerrar la ultima llave
-var nombreRec=$("#nombreRec").val();
-var emailRec=$("#emailRec").val();
-var direccionRec=$("#direccionRec").val();
-
 $(document).ready(function () {
-    $("#agregarCarrito").click(function (e) { 
-        e.preventDefault();
-        var id=$(this).data('id');
-        var nombre=$(this).data('nombre');
-        var web_path=$(this).data('web_path');
-        var cantidad=$("#cantidadProducto").val();
-        $.ajax({
-            type: "post",
-            url: "ajax/agregarCarrito.php",
-            data: {"id":id,"nombre":nombre,"web_path":web_path,"cantidad":cantidad},
-            dataType: "json",
-            success: function (response) {
-                var cantidad=Object.keys(response).length;
-                $("#badgeProducto").text(cantidad);
-            }
-        });
-    });
-});
-
-$("#jalar").click(function(e){
-    var nombreCli=$("#nombreCli").val();
-    var emailCli=$("#emailCli").val();
-    var direccionCli=$("#direccionCli").val();
-=======
-$(document).ready(function () {
->>>>>>> master
-
     $.ajax({
         type: "post",
         url: "ajax/leerCarrito.php",
@@ -96,5 +63,23 @@ $(document).ready(function () {
             $("#emailRec").val(emailRec);
             $("#direccionRec").val(direccionRec);
         }
+    });
+
+    $("#agregarCarrito").click(function (e) { 
+        e.preventDefault();
+        var id=$(this).data('id');
+        var nombre=$(this).data('nombre');
+        var web_path=$(this).data('web_path');
+        var cantidad=$("#cantidadProducto").val();
+        $.ajax({
+            type: "post",
+            url: "ajax/agregarCarrito.php",
+            data: {"id":id,"nombre":nombre,"web_path":web_path,"cantidad":cantidad},
+            dataType: "json",
+            success: function (response) {
+                var cantidad=Object.keys(response).length;
+                $("#badgeProducto").text(cantidad);
+            }
+        });
     });
 });
