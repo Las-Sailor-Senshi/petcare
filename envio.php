d918eb2b37bd683d6e7037bbaa647fb060221deb
dxdiag41 <77176041+dxdiag41@users.noreply.github.com>
2021-03-11 02:48:44 -0600

    envio

--- envio.php
+++ envio.php
@@ -56,50 +56,13 @@ if (isset($_SESSION['idCliente'])) {
                                 </tbody>
                             </table>
                         </div>
-                        <!-- /.card-body -->
-                    </div>
-                    <!-- /.card -->
-
-                    <!-- /.card -->
-                </div>
-                <!-- /.col -->
-                        <div class="form group">
-                            <label for="">Direccion</label>
-                            <textarea name="direccionRec" id="direccionRec" class="form-control" row="3"></textarea>
-                        </div>
-                    </div>
-
-                    <div class="col-6">
-                        <h3>Datos de quien recibe</h3>
-                        <div class="form group">
-                            <label for="">Nombre</label>
-                            <input type="text" name="nombreCli" id="nombreCli" class="form-control">
-                        </div>
-                        <div class="form group">
-                            <label for="">Email</label>
-                            <input type="email" name="emailCli" id="emailCli" class="form-control">
-                        </div>
-                        <div class="form group">
-                            <label for="">Direccion</label>
-                            <textarea name="direccionCli" id="direccionCli" class="form-control" row="3"></textarea>
-                        </div>
-                        <div class="form-check">
-                            <label class="form-check-label">
-                                <input type="checkbox" class="form-check-input" id="jalar">
-                                Jalar datos del cliente
-                            </label>
-                        </div>
                     </div>
                 </div>
             </div>
-            <!-- /.row -->
         </div>
-        <!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-    <a name="" id="" class="btn btn-warning" href="index.php?modulo=carrito" role="button">Regresar a carrito</a>
-    
-<?php
+    </section>    
+    <a name="" id="" class="btn btn-warning" href="index.php?modulo=carrito" role="button">Regresar a carrito</a>    
+<?php                        
 } else {
 ?>
     <section class="content-header">
