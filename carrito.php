935137ced6776dee0d42461825e493667c84d993
Samuel Medina <68279149+MedinaSamuel@users.noreply.github.com>
2021-03-11 02:06:46 -0600

    carrito

--- carrito.php
+++ carrito.php
@@ -1,7 +1,6 @@
 <table class="table table-striped table-inverse" id="tablaCarrito" >
     <thead class="thead-inverse">
         <tr>
-            <th>Imagen</th>
             <th>Nombre</th>
             <th>Cantidad</th>
             <th>Precio</th>
--- menu.php
+++ menu.php
@@ -1,12 +1,9 @@
 <!-- Navbar -->
-<!-- Quitar main-header y barraLateral para quitar espacio vacio de boton de categorias--> 
   <nav class="main-header navbar navbar-expand navbar-dark">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
         <!-- BarraLateral -->
-        <li class="nav-item">
-            <a class="nav-link nav-item navbar-nav" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-        </li>       
+           
         <li class="nav-item d-none d-sm-inline-block">
             <a href="index.php" class="nav-link">Inicio</a>
         </li>
